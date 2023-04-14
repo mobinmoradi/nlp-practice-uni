1aca9123aeef98ebd9a8cd01c428d078e7626ee4
mobin moradi <mobinmoradimail@gmail.com>
2023-04-15 03:46:52 +0430

    tfIdf

---
+++ TfIdf.js
@@ -0,0 +1,72 @@
+function TfIdf() {
+}
+
+TfIdf.prototype.weights = function(documents,term) {
+    
+    var results = []
+    
+    var idf = this.idf(documents,term)
+    
+    for(var i=0;i<documents.length;i++) {
+        
+        var tf = this.tf(documents[i],term)
+        var tfidf = tf*idf
+        var result = tfidf  
+        
+        results.push(result)
+    }
+
+    return results
+}
+
+TfIdf.prototype.tf = function(words,term) {
+
+    var result = 0
+    
+    for(var i=0;i<words.length;i++) {
+
+        var word = words[i]
+
+        if(word.indexOf(term)!=-1) {
+            result = result+1
+        }    
+    }
+
+    return result/words.length
+}
+
+TfIdf.prototype.idf = function(documents,term) {
+   
+    var occurence = 0
+
+    for(var j=0;j<documents.length;j++) {
+        
+        var doc = documents[j]
+        
+        if(this.__wordInsideDoc(doc,term)){
+            occurence = occurence+1
+        }                  
+    }
+
+    if(occurence==0) {
+        return undefined    
+    }
+
+    return Math.log(documents.length/occurence)
+}
+
+TfIdf.prototype.__wordInsideDoc = function(doc,term) {
+    
+    for(var i=0;i<doc.length;i++) {
+
+        var word = doc[i]
+
+        if(word.indexOf(term)!=-1) {
+            return true
+        }
+    }    
+
+    return false
+}
+
+module.exports = TfIdf
--- index.js
+++ index.js
@@ -19,13 +19,45 @@ stopwords.build(stopWords);
 
 var tagger = new pos.Tagger();
 
+var TfIdf = require('./TfIdf')
+
+var tfIdf = new TfIdf()
+
+let array = [];
+let arrayOfWords = []
+
 data.forEach(element => {
-    const result1 = normalizer.normalize(element);
-    const result2 = tokenizer.tokenize(result1);
+
+    const result1 = normalizer.normalize(element)
+    let removeNumbers = result1.replace(/[0-9]/g, '');
+
+    const result2 = tokenizer.tokenize(removeNumbers);
+
     const result3 = stopwords.removeStopwords(result2)
     const result4 = result3.map(el => stemmer.stemWord(el));
-    const result5 = tagger.tag(result4);
-    console.log(result5);
+
+    array.push(result3)
+
+    result4.forEach(element => {
+        if(!arrayOfWords.includes(element)) arrayOfWords.push(element)
+    });
+
+    // const result5 = tagger.tag(result4);
+
 });
 
+let final = []
+
+arrayOfWords.forEach(element => {
+    final.push([element,tfIdf.weights(array,element)])
+});
+
+
+const jsonexport = require('jsonexport');
+
+jsonexport(final, function(err, csv){
+    if (err) return console.error(err);
+    
+    fs.appendFileSync('./contacts.csv', csv.replace(/;/g, ','));
+});
 
